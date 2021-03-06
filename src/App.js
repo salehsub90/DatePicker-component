fb43907fe51d414c12bb0fc235d1dfd6e50b25b4
Saleh Suboh <subohsaleh@gmail.com>
2021-03-06 03:06:43 -0500

    Date range and single date selection implemented with useState hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
+import { DateRange, DateRangePicker } from 'react-date-range';
 import "react-datepicker/dist/react-datepicker.css";
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css'; 
 import './App.css';
 
 
 function App() {
 
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection',
+    }
+  ]);
 
   return (
     <div className="App">
+      <span>Single:</span>< br />
       <DatePicker
         selected={selectedDate}
         onChange={date => setSelectedDate(date)}
@@ -18,6 +29,15 @@ function App() {
         isClearable={true}
         showYearDropdown
       />
+      <br />
+      <span>Range:</span>< br />
+      <DateRange
+        editableDateInputs={true}
+        onChange={item => setState([item.selection])}
+        moveRangeOnFirstSelection={false}
+        ranges={state}
+      />
+      
     </div>
   );
 }
